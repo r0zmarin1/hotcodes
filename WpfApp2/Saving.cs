0af0be967c07bc4c0ab1b44be972331f84a1d0fe
r0zmarin1 <irozina2006@gmail.com>
2024-01-30 01:24:00 +1000

    fyjjucgyhjcfyyjh

---
+++ WpfApp2/Saving.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    class Saving
+    {
+            private Saving() { }
+            public string Login { get; set; }
+            public string Password { get; set; }
+            static Saving instance;
+            static object locker = new object();
+            public static Saving GetInstance()
+            {
+                lock (locker)
+                {
+                    if (instance == null) instance = new Saving();
+                    return instance;
+                }
+            }       
+    }
+    
+}
+
